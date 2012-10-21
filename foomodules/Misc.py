2bd4f3daac81a5e042fa9187b6743862a66dff22
Jonas Wielicki <j.wielicki@sotecware.net>
2012-10-21 17:36:25 +0200

    Numeric document matcher
    Can be used to match for rfc-1234 and load an url by parsing the number
    out of the string and fitting it in some url template.

--- foomodules/Misc.py
+++ foomodules/Misc.py
@@ -1,4 +1,5 @@
 import foomodules.Base as Base
+import foomodules.URLLookup as URLLookup
 
 class Pong(Base.MessageHandler):
     def __call__(self, msg, errorSink=None):
@@ -19,4 +20,30 @@ class IgnoreList(Base.MessageHandler):
             return
         self.ignoredJids.add(bare)
         self.reply(msg, self.message)
+
+
+class NumericDocumentMatcher(Base.MessageHandler):
+    def __init__(self, document_regexes, url_lookup, **kwargs):
+        super().__init__(**kwargs)
+        self.document_regexes = document_regexes
+        self.url_lookup = url_lookup
+
+    def __call__(self, msg, errorSink=None):
+        contents = msg["body"]
+
+        for regex, document_format in self.document_regexes:
+            for match in regex.finditer(contents):
+                groups = match.groups()
+                groupdict = match.groupdict()
+                document_url = document_format.format(*groups, **groupdict)
+
+                try:
+                    iterable = iter(self.url_lookup.processURL(document_url))
+                    first_line = next(iterable)
+                    self.reply(msg, "<{0}>: {1}".format(document_url, first_line))
+                    for line in iterable:
+                        self.reply(msg, line)
+                except URLLookup.URLLookupError as err:
+                    self.reply(msg, "<{0}>: sorry, couldn't look it up: {1}".format(document_url, str(err)))
+                    pass
 
