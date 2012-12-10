61b1ae6d9dde9654ce4fa1f0dd4e43a30257b67a
Jonas Wielicki <j.wielicki@sotecware.net>
2012-12-10 14:31:49 +0100

    Properly detect evil chars

--- foomodules/utils.py
+++ foomodules/utils.py
@@ -3,4 +3,4 @@ def cleanup_string(s):
     return "".join(filter(control_character_filter, s))
 
 def evil_string(s):
-    return any((ord(x) < 32 and x != b"\x0A" and x != b"\x0D") for x in s)
+    return any((ord(x) < 32 and x != "\x0A" and x != "\x0D") for x in s)
