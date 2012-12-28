9a85555a9036693deadad289ee74585196309a5a
Jonas Wielicki <j.wielicki@sotecware.net>
2012-12-28 09:08:45 +0100

    Only divide by unit if neccessary

--- sympy-daemon.py
+++ sympy-daemon.py
@@ -21,28 +21,34 @@ if __name__ == "__main__":
 
     sock = socket.fromfd(args.fd, socket.AF_UNIX, socket.SOCK_STREAM)
 
+    one = sympify("1")
     u.i = sympy.sqrt(-1)
-    while True:
-        unit, expr = SympyComm.recv_calc(sock)
-        try:
-            unit = sympy.sympify(unit, locals=u.__dict__)
-        except Exception as err:
-            SympyComm.send_error(sock, "could not sympify unit: {}".format(str(err)).encode("utf-8"))
-            continue
-        try:
-            expr = sympy.sympify(expr, locals=u.__dict__)
-        except Exception as err:
-            SympyComm.send_error(sock, "could not sympify expression: {}".format(str(err)).encode("utf-8"))
-            continue
+    try:
+        while True:
+            unit, expr = SympyComm.recv_calc(sock)
+            try:
+                unit = sympy.sympify(unit, locals=u.__dict__)
+            except Exception as err:
+                SympyComm.send_error(sock, "could not sympify unit: {}".format(str(err)).encode("utf-8"))
+                continue
+            try:
+                expr = sympy.sympify(expr, locals=u.__dict__)
+            except Exception as err:
+                SympyComm.send_error(sock, "could not sympify expression: {}".format(str(err)).encode("utf-8"))
+                continue
 
-        try:
-            result = expr / unit
-        except Exception as err:
-            SympyComm.send_error(sock, b"during evaluation: "+str(err).encode("utf-8"))
-        try:
-            SympyComm.send_result(sock, str(float(result / unit)).encode("ascii"))
-        except ValueError as err:
-            SympyComm.send_result(sock, str(result).encode("ascii"))
-        except Exception as err:
-            SympyComm.send_error(sock, str(err).encode("utf-8"))
-
+            result = expr
+            if unit != one:
+                try:
+                    result = expr / unit
+                except Exception as err:
+                    SympyComm.send_error(sock, b"during evaluation: "+str(err).encode("utf-8"))
+            try:
+                SympyComm.send_result(sock, str(float(result / unit)).encode("ascii"))
+            except ValueError as err:
+                SympyComm.send_result(sock, str(result).encode("ascii"))
+            except Exception as err:
+                SympyComm.send_error(sock, str(err).encode("utf-8"))
+    finally:
+        sock.shutdown(socket.SHUT_RDWR)
+        sock.close()
