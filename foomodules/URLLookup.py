8eb94dc3ec58618530c3573bc471a8c3e1b6086f
Jonas Wielicki <j.wielicki@sotecware.net>
2013-02-17 14:34:17 +0100

    Special-handle utf-8lias encoding (hi dilbert)

--- foomodules/URLLookup.py
+++ foomodules/URLLookup.py
@@ -236,6 +236,10 @@ class HTMLDocument(HandlerBase):
                 encoding = groups[1] or groups[2] or groups[3]
                 response.encoding = encoding.decode("ascii")
 
+        if response.encoding == "utf-8lias":
+            # hi dilbert.com
+            response.encodin = "utf-8"
+
         if bufferLen < response.contentLength:
             try:
                 contents, _ = guessEncoding(response.buf, response.encoding)
