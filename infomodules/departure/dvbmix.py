2ce592e24b35753ace0221414d7f0eee7e1446a3
Jonas Wielicki <j.wielicki@sotecware.net>
2013-11-28 16:11:57 +0100

    This is python3

--- infomodules/departure/dvbmix.py
+++ infomodules/departure/dvbmix.py
@@ -77,7 +77,7 @@ class Departure(object):
         # if you come across this, sorry for the hack
         # feel free to remove it. i need it to collect some data
         with open("/home/xmpp-crowd/dvbmix/{}.log".format(stop_name), "a") as dump:
-            dump.write("{} {}\n".format(get_timestamp(), contents).encode())
+            dump.write("{} {}\n".format(get_timestamp(), contents))
 
         self.cached_data[stop_name] = stop_filter.filter_departures(
             self.parse_data(contents))
