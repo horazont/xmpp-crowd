7338cafd0a0858dd0a7e7e56161b819d658bf920
Jonas Wielicki <j.wielicki@sotecware.net>
2014-07-20 19:52:13 +0200

    Implement auth supplier

--- foomodules/link_harvester/__init__.py
+++ foomodules/link_harvester/__init__.py
@@ -9,6 +9,7 @@ import sqlalchemy.exc
 import sqlalchemy.orm.exc
 
 import muclinks
+import muclinks.model
 
 logger = logging.getLogger(__name__)
 
@@ -55,3 +56,46 @@ class LinkHarvester(Base.XMPPObject):
         except Exception as err:
             logger.exception(err)
 
+
+class AuthTokenSupplier(Base.ArgparseCommand):
+    def __init__(self,
+                 controller,
+                 url_format,
+                 command_name="!auth",
+                 **kwargs):
+        super().__init__(command_name, **kwargs)
+        self.controller = controller
+        self.url_format = url_format
+
+    def __call__(self, msg, arguments, errorSink=None):
+        senderjid = self.XMPP.muc.getJidProperty(
+            msg["from"].bare,
+            msg["from"].resource,
+            'jid')
+
+        senderjid = str(senderjid.bare)
+
+        if not senderjid:
+            self.reply(msg, "could not determine your JID")
+            return
+
+        sendernick = msg["from"].resource
+
+        with self.controller.with_new_session() as ctx:
+            account = ctx.get_account_for_jid(
+                senderjid,
+                nick=sendernick)
+            if account is None:
+                self.reply(msg,
+                           "no account for this jid: {}".format(
+                               senderjid))
+                return
+
+            key = ctx.create_ota_key(account)
+            print(account.id)
+
+        self.reply(
+            msg,
+            self.url_format.format(
+                key=muclinks.model.key_to_str(key)),
+            overrideMType="chat")
